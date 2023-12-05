c834008be8a45f674e56e0b27b3648fcfbd0d61b
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2023-12-05 02:10:34 +0200

    New_Commit

--- client/src/components/public/index.js
+++ client/src/components/public/index.js
@@ -1,5 +1,5 @@
-export * from "./form";
-export * from "./Table.jsx";
+export * from "./Form";
+export * from "./Table";
 export * from "./Input";
 export * from "./Select";
 export * from "./Switch";
