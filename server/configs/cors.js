388cf7836e759ee42a3dfb26772d63d34cb9d061
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2023-12-25 13:26:02 +0200

    New_Commit

--- server/configs/cors.js
+++ server/configs/cors.js
@@ -1,19 +1,14 @@
-const localHosts = [
-	"http://localhost:*",
-	"http://localhost:5000",
+const whiteList = [
 	"http://localhost:5173",
-	"http://localhost:5174",
+	"http://localhost:8000",
+	"https://cars-management-client.vercel.app",
+	"https://cars-management-client.netlify.app",
 ];
-const vercel = ["https://cars-management-client.vercel.app"];
-const netlify = ["https://cars-management-client.netlify.app"];
-
-const whiteList = localHosts.concat(vercel, netlify);
 
 export const corsOrigins = {
 	credentials: true,
 	optionsSuccessStatus: 200,
 	origin: (origin, callback) => {
-		// origin = undefined -> For localhost
 		const isAcceptable = whiteList.some((site) => site === origin);
 		if (isAcceptable || origin === undefined) callback(null, origin);
 		else callback(`${origin}: Not Allowed By CORS`);
