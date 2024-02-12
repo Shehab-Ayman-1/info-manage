c315e8e941e2926926f2419838893528e7cd25b5
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2024-02-12 21:56:25 +0200

    QUECK_COMMIT

--- server/middlewares/verify.middleware.js
+++ server/middlewares/verify.middleware.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const SERVER_RESTART = async () => {
+export const SERVER_RESTART = async (req, res, next) => {
 	try {
 		setInterval(async () => await axios.get("https://info-manage-client.vercel.app"), 1000 * 30);
+		next();
 	} catch (error) {}
 };
