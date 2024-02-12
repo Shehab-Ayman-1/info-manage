06dd5c244724b66eb8d83788d8bc3bc46d410b8d
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2024-02-13 00:50:32 +0200

    QUECK_COMMIT

--- client/src/layout/404.jsx
+++ client/src/layout/404.jsx
@@ -1,11 +1,9 @@
 export const PageNotFound = () => {
+   const src = "https://webpresence.digital/wp-content/uploads/2012/05/google-penguin-optimization.jpg";
+   const className = "absolute left-1/2 top-1/2 mx-auto block -translate-x-1/2 -translate-y-1/2 object-contain";
    return (
       <div className="min-h-[calc(100vh-115px)]">
-         <img
-            src="https://webpresence.digital/wp-content/uploads/2012/05/google-penguin-optimization.jpg"
-            alt="404 page"
-            className="absolute left-1/2 top-1/2 mx-auto block -translate-x-1/2 -translate-y-1/2 object-contain"
-         />
+         <img src={src} alt="404 page" className={className} />
       </div>
    );
 };
